58a480dbdd023590748daade6827ec1f31b569c5
Alekseyua <alekseyuadnepr@gmail.com>
2023-12-03 12:42:02 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import axios from 'axios';
+
+
+const PORT = process.env.NODE_PORT || 4444;
+
+
+const app = express();
+
+app.get('/', async (req, res)=>{
+  try {
+    const url_image = req?.query?.url_image;
+    if (url_image){
+
+      const responseAxios = await axios.get(url_image,{
+        responseType: 'arraybuffer'
+      })
+      const b64 = Buffer.from(await responseAxios.data, 'binary').toString('base64');
+      const mimeType = `image/${url_image.split('.').pop()}`      
+      res.status(200).send(`<img src="data:${mimeType};base64,${b64}" />`);
+    }else{
+      res.send('Something occurred wrong !!!')
+    }
+    
+  } catch (error) {
+    console.log(error)
+    res.sendStatus(500)
+  }
+})
+
+app.listen(PORT, ()=>console.log(`Lisen port ${PORT}`))
